4f8b415bd069d52c49e4be4edb45e0359650ee53
Vikas Srivastava <er.vikassri@gmail.com>
2019-06-25 11:54:55 +0530

    Update sample.py

--- examples/sample.py
+++ examples/sample.py
@@ -2,7 +2,7 @@ from cbconnect import *
 
 
 # connecting the class 
-client = cbconnect(cbhost="hw-cb-dev01.us-east4.us.walmart.net",cbuser="hw-cb-hadoop@wal-mart.com", cbpass="3DLC3DLP")
+client = cbconnect(cbhost="hostname",cbuser="useremail", cbpass="userpass")
 
 # listong the workspaces
 client.ListWorkSpaces()
